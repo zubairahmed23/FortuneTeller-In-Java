20ed601cb717322734ecabde69bfa7f824c11584
Sheikh Zubair Ahmed <134803474+zubairahmed23@users.noreply.github.com>
2024-01-14 21:38:43 +0530

    first commit

---
+++ FortuneTeller/FortuneTeller.java
@@ -0,0 +1,23 @@
+//Written by Sheikh Zubair Ahmed
+import java.util.Scanner;
+import java.util.Random;
+
+public class FortuneTeller{
+    public static void main(String[]argstell
+    ){
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Enter a question:");
+        String question = sc.nextLine();
+        sc.close();
+
+        String[] predictions = {"Yes", "No", "Ask again later", "Cannot predict now", "Don't count on it", "Most likely", "Outlook not so good", "Signs point to yes", "Definitely not", "Maybe", "Concentrate and ask again", "My sources say no"};
+        int randomInt = random.nextInt(12);
+        String prediction = predictions[randomInt];
+
+        System.out.println("--------------------------------");
+        System.out.println("Question = "+question);
+        System.out.println("My Prediction = "+prediction);
+    }
+}
